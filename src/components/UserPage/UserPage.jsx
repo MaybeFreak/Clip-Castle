b4e62fb9ddd287b22d8826b7434d399571ea0b49
MaybeFreak <michaelpope97@gmail.com>
2023-06-05 17:14:10 +0200

    setup initial userpage:

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,21 +1,53 @@
 import { onAuthStateChanged } from "firebase/auth";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { auth } from "../../firebase";
+import { auth, db } from "../../firebase";
+import UserInfo from "../UserInfo/UserInfo";
+import ClipCard from "../ClipCard/ClipCard";
+import { collection, getDocs, limit, query, where } from "firebase/firestore";
 
 function UserPage() {
+  const [clips, setClips] = useState(false);
   const navigate = useNavigate();
   const params = useParams();
+
+  const fetchClips = async (user) => {
+    const q = query(
+      collection(db, "clips"),
+      where("Owner", "==", user),
+      limit(5)
+    );
+    const querySnapshot = await getDocs(q);
+
+    const data = [];
+    querySnapshot.forEach((doc) => {
+      data.push({ id: doc.id, data: doc.data() });
+    });
+
+    setClips(data);
+  };
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (params.id === user.uid) navigate("/profile");
     });
+    fetchClips(params.id);
   }, []);
 
   return (
     <>
-      <div>This is the UserPage</div>
+      <main>
+        <div className="infoAndClips">
+          <UserInfo userId={params.id} />
+          <div>
+            <h2>Clips</h2>
+            <div className="clips">
+              {clips &&
+                clips.map((clip, i) => <ClipCard key={i} clip={clip} />)}
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
